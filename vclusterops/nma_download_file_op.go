0950aaa0e354815f4d38bef6c238c95e37828a5f
rs-vertica-infodev <83075392+rs-vertica@users.noreply.github.com>
2023-12-11 15:41:33 -0500

    Update nma_download_file_op.go

--- vclusterops/nma_download_file_op.go
+++ vclusterops/nma_download_file_op.go
@@ -69,7 +69,7 @@ func (e *ClusterLeaseNotExpiredError) Error() string {
 }
 
 // ReviveDBNodeCountMismatchError is the error that is returned when the number of
-// nodes in the new cluster does not match the number of nodes in the original cluster.
+// nodes in the revived cluster does not match the number of nodes in the original cluster.
 type ReviveDBNodeCountMismatchError struct {
 	ReviveDBStep  string
 	FailureHost   string
