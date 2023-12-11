c6cff3b91f1294fc18c09c4ad2664d351925911a
rs-vertica-infodev <83075392+rs-vertica@users.noreply.github.com>
2023-12-11 13:08:23 -0500

    Update cluster_config.go
    read and write config doc update.

--- vclusterops/cluster_config.go
+++ vclusterops/cluster_config.go
@@ -66,7 +66,8 @@ func MakeDatabaseConfig() DatabaseConfig {
 	return DatabaseConfig{}
 }
 
-// read config information from the YAML file
+// ReadConfig reads cluster configuration information from a YAML-formatted file in configDirectory.
+// It returns the a ClusterConfig and any error encountered when reading and parsing the file.
 func ReadConfig(configDirectory string, logger vlog.Printer) (ClusterConfig, error) {
 	configFilePath := filepath.Join(configDirectory, ConfigFileName)
 	configBytes, err := os.ReadFile(configFilePath)
@@ -103,7 +104,8 @@ func ReadConfig(configDirectory string, logger vlog.Printer) (ClusterConfig, err
 	return clusterConfig, nil
 }
 
-// write config information to the YAML file
+// WriteConfig writes configuration information to configFilePath.
+// It returns any write error encountered.
 func (c *ClusterConfig) WriteConfig(configFilePath string) error {
 	var config Config
 	config.Version = CurrentConfigFileVersion
